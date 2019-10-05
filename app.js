11658e6cbf8d9ed9559960bbc9a90ad9eada64ad
Iliyan Stefanov <iliqni6e@abv.bg>
2019-10-05 13:50:19 +0300

    Get input data and set eventlistener.

--- app.js
+++ app.js
@@ -3,19 +3,39 @@ var budgetController = (function() {})();
 
 //UI CONTROLLER
 var UIController = (function() {
-  // some code
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  return {
+    getinput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc(for income) or exp(for expenses)
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function() {
     // 1.Get the input value
+    var input = UICtrl.getinput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
